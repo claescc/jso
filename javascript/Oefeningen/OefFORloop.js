ae6149d82d764df6fdcb761b04c336378ba83696
claescc <claes_cc@live.be>
2017-10-24 17:16:36 +0200

    Oef Liedje beer on the wall !

---
+++ javascript/Oefeningen/OefFORloop.js
@@ -0,0 +1,23 @@
+
+'use strict';
+
+var toetsenbord = require('readline-sync');
+var startGetal = parseInt(toetsenbord.question("Geef een startgetal: ")); 
+var container = toetsenbord.question("Geef een container: "); 
+var consumptie = toetsenbord.question("Geef een consumptie artikel: "); 
+var teller = 0;
+var aantalZuip = startGetal
+
+for (var teller = 1;                        // for teller := 1
+     teller <= startGetal;                  // to startgetal
+     teller++)                              // teller + 1  (dan telt uw teller elke keer op)
+{  
+    console.log(aantalZuip + " " + container + " of " + consumptie + " on the wall " + aantalZuip + " " + container +  " of " + consumptie );
+    // write song 
+    aantalZuip -= 1;
+    // minus one 
+    console.log("Take one down and pass it around, " + aantalZuip + " " + container + " of " + consumptie + " on the wall.");
+    // write second verse                 
+    
+}
+
