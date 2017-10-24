a2900bd5fda7462bd5e771ce2b4ee98dacd04fc7
claescc <claes_cc@live.be>
2017-10-24 11:49:14 +0200

    Switch BOOLEAN case true &&

--- javascript/Oefeningen/Oef2SwitchCASE.js
+++ javascript/Oefeningen/Oef2SwitchCASE.js
@@ -1,6 +1,35 @@
 'use strict';
+
+console.log('Oefening SWITCH GRAAD');
+
+
+var toetsenbord = require('readline-sync'); 
+var ingaveA =  parseInt(toetsenbord.question("Geef eerste getal: "), 10);
+
+
+
+switch (true) {                 
+   case (ingaveA = 0 && ingaveA <=9 ) :               
+       console.log("ovoldoende");
+       break;            
+   case (ingaveA = 10 && ingaveA <=13 ) : 
+       console.log("voldoende");
+       break;          
+   case (ingaveA = 14 && ingaveA <=15 ) : 
+       console.log("onderscheiding");
+       break;   
+    case (ingaveA = 16 && ingaveA <=17 ) : 
+       console.log("grote onderscheiding");
+       break  
+   case (ingaveA = 18 && ingaveA <=20 ) : 
+       console.log("grootste onderscheiding ");
+    default: 
+         console.log("ongeldige Score ");
+        break;
+} 
+/*
  
-console.log('Oefening SWITCH CASE ');
+console.log('Oefening SWITCH CASE + - * / ');
 
 
 var toetsenbord = require('readline-sync'); 
@@ -29,3 +58,4 @@ switch (operator) {
     default:
         console.log("Foutieve operator moest * / - + zijn ");
 } 
+*/
