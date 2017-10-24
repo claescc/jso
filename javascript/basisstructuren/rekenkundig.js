d7c1cc989f3f368b806e10206237ca18b901bb0e
claescc <claes_cc@live.be>
2017-10-24 10:05:08 +0200

    Som++, IF, ELSE, && , ||, ==, ===

--- javascript/basisstructuren/rekenkundig.js
+++ javascript/basisstructuren/rekenkundig.js
@@ -5,19 +5,25 @@ var som;
 var resultaat1, resultaat2;
 
 som = x + y;
-console.log(som);
+console.log(som); // 3
 som = som + x;
-console.log(som);
+console.log(som); // 4
 som += x;
-console.log(som);
-som++;
-console.log(som);
-console.log(som++);
-console.log(som);
-console.log(++som);
-console.log(som);
+console.log(som); // 5
+som++;              // positie van ++ is belangrijk !!
+console.log(som); // 6
+//  ++ : increment 
+console.log(som++); //6 // POSTFIX  
+//som is 6 en pas daarna gaat waarde verhoogd worden. dus 7
 
-resultaat1 = x + y * z;
-console.log("zonder haakjes : " + resultaat1);
+console.log(som); // 7
+console.log(++som);// 8 // PREFIX
+console.log(som); // 8
+
+// Delen / en product * voorang op plus  + en min - 
+resultaat1 = x + y * z; 
+console.log("zonder haakjes : " + resultaat1); // 7 
+
+// RONDE HAAKJES hebben voorrang op wiskundige regels !
 resultaat2 = (x + y) * z;
-console.log("met haakjes : " + resultaat2);        
+console.log("met haakjes : " + resultaat2);  // 9 
