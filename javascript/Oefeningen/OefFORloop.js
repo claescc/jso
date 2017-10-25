993210f16eeb28ae281a4955d047f941e3ec3f5e
claescc <claes_cc@live.be>
2017-10-25 11:01:51 +0200

    Oefeningen for while / while for loop / Vraagteken operator ??

--- javascript/Oefeningen/OefFORloop.js
+++ javascript/Oefeningen/OefFORloop.js
@@ -61,12 +61,20 @@ VRAAG 5 GETALLEN
  //var getalA = parseInt(toetsenbord.question("Geef getal ")); 
  var som = 0;
 
- for (var teller = 1; teller <= aantalGetallen; teller++) 
- {
+
+ while (aantalGetallen != ) {
+     
+ }
+     for (var teller = 1; teller <= aantalGetallen; teller++) 
+     {
     var getalB = parseInt(toetsenbord.question("Geef volgend getal in "));
     som = som+getalB;
     //getalA = 0; 
- }
- console.log("De som van de 5 getallen is = %s",som); 
+    }
+    console.log("De som van de 5 getallen is = %s",som); 
+
+
+
+
  
 
