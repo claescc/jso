8a1a892c2cf1c1025530a83ab21dbdf6ce633643
claescc <claes_cc@live.be>
2017-10-25 13:47:45 +0200

    oefenigen functies

--- javascript/Oefeningen/oefFUNCTIE.js
+++ javascript/Oefeningen/oefFUNCTIE.js
@@ -13,7 +13,7 @@ function berekenMecht(grondtal, exponent){    // lus maken voor exponent keer ma
 */
 //______________________________________________________________________
 // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-/*
+
 
 'use strict';
 
@@ -74,3 +74,96 @@ function max(x, y) {
                                        
     return result;            // return zorgt ervoor dat functie gedaan is end calc  
 } 
+*/
+
+// CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC 
+
+'use strict';
+
+var toetsenbord = require('readline-sync');
+var resultaat;
+
+var x = toetsenbord.question("Geef eerste getal: ");
+
+
+resultaat = BEFNaarEuro(x);
+console.log("Belgische frank is = Euro %s", resultaat);
+
+
+
+function BEFNaarEuro(BEF) {             
+    var result = 1;                         // maak variabel van WISSELKOERS
+      result = BEF/40.3399                                        
+    return result;             
+}
+
+*/
+// DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
+
+'use strict';
+
+var toetsenbord = require('readline-sync');
+var resultaat;
+
+var x = toetsenbord.question("Geef prijs van brood in euro : ");
+var y = toetsenbord.question("Geef prijs van melk in euro : ");
+
+resultaat = (  (EuroNaarBEF(x))    +   (EuroNaarBEF(y))    );
+console.log("euro zou = Belgische Frank %s", resultaat);
+
+function EuroNaarBEF(EURO) {             
+    var result = 1;           
+      result = EURO*40.3399                                        
+    return result;             
+}
+*/
+// EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+
+'use strict';
+
+var toetsenbord = require('readline-sync');
+var resultaat;
+
+var x = toetsenbord.question("celsius in: ");
+
+resultaat = celsius (x)
+console.log("Fahrenheit = ", resultaat);
+
+function celsius(x) {             
+    var result = 1;           
+      var c = 5.0/9.0*(x - 32);
+      result = c;                                      
+    return result;             
+}
+*/
+//FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+
+
+'use strict';
+
+var toetsenbord = require('readline-sync');
+var resultaat;
+
+var x = toetsenbord.question("Fahrenheit in: ");
+var y = toetsenbord.question("celsius in: ");
+
+resultaat = fahrenheit (x)
+console.log("is Celsius = ", resultaat);
+
+resultaat = celsius (y)
+console.log("Fahrenheit = ", resultaat);
+// ___________________________________________________________________________
+
+function celsius(x) {             
+    var result = 1;           
+      var c = 5.0/9.0*(x - 32);
+      result = c;                                      
+    return result;             
+}
+
+function fahrenheit(y) {             
+    var result = 1;           
+      var f = 9.0/5.0*y + 32; 
+      result = f;                                      
+    return result;             
+}
