0b5fe1db4cb3bcafc5b199951e3e2de3729a3855
claescc <claes_cc@live.be>
2017-10-25 10:21:41 +0200

    OefForLoop!!

--- javascript/Oefeningen/OefFORloop.js
+++ javascript/Oefeningen/OefFORloop.js
@@ -1,4 +1,6 @@
-
+/*_________________________________________________________________
+BOTTLES OF BEEER
+ _________________________________________________________________/ 
 'use strict';
 
 var toetsenbord = require('readline-sync');
@@ -20,4 +22,51 @@ for (var teller = 1;                        // for teller := 1
     // write second verse                 
     
 }
+*/
+
+
+/*_________________________________________________________________
+MAAL TAFELS 
+ _________________________________________________________________/
+
+ 'use strict';
+ 
+ var toetsenbord = require('readline-sync');
+ var maalTafels = parseInt(toetsenbord.question("Geef aantal maaltafels in: ")); 
+ var aantal = 10;
+ var som;
+ var teller = 0;
+ var tafel = 0
+
+ for (var teller = 0; teller <= maalTafels ; teller++) { // hoeveel tafels laten we zien
+   
+    var tafel = tafel + 1           // dit moet 5 keer gaan 
+    for (var teller = 0; teller <= aantal; teller++) 
+    {
+        som = (teller*tafel)
+        console.log("%s x %s = %s\n", teller, tafel, som); 
+    }
+
+     
+ }
+ */
+/*_________________________________________________________________
+VRAAG 5 GETALLEN
+ _________________________________________________________________*/
+
+ 'use strict';
+ 
+ var toetsenbord = require('readline-sync');
+ var aantalGetallen = parseInt(toetsenbord.question("Aantal getallen: ")); 
+ //var getalA = parseInt(toetsenbord.question("Geef getal ")); 
+ var som = 0;
+
+ for (var teller = 1; teller <= aantalGetallen; teller++) 
+ {
+    var getalB = parseInt(toetsenbord.question("Geef volgend getal in "));
+    som = som+getalB;
+    //getalA = 0; 
+ }
+ console.log("De som van de 5 getallen is = %s",som); 
+ 
 
