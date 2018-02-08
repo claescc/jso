c5257a2731853dfb09f07d06c962aba8265e72cc
claescc <claes_cc@live.be>
2018-02-08 09:13:19 +0100

    VB00

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,16 +1,22 @@
+//CONFIGURATIE APP.MODULE.TS
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { VbDatabindingComponent } from './voorbeelden/vbdatabinding/vbdatabinding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    VbDatabindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [VbDatabindingComponent]
 })
 export class AppModule { }
