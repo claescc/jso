1499087b286c4253ea17959f8b68d3075286567d
claescc <claes_cc@live.be>
2018-02-09 09:58:50 +0100

    brdoojes complete {{ totaal }}

--- angular/src/app/broodjes/broodjes.component.ts
+++ angular/src/app/broodjes/broodjes.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./broodjes.component.css']
 })
 export class BroodjesComponent implements OnInit {
- 
   broodjes = [{naam: "Ham", prijs: 2.30},
               {naam: "Krab", prijs: 2.60},
               {naam: "Kaas", prijs: 1.70},
@@ -16,32 +15,22 @@ export class BroodjesComponent implements OnInit {
   teller: number;
   lijst= [];
 
-  constructor() {}
+  constructor() { }
   ngOnInit() { }
 
   bestellen(){
-    
-    let broeike = new Broodje(this.broodjes[this.val].naam, 
-                              this.broodjes[this.val].prijs);
-    
-    //console.log('broodje: ',this.broodjes[this.val].naam,this.broodjes[this.val].prijs);
-
+    let broeike = new Broodje(this.broodjes[this.val].naam,  // Krab
+                              this.broodjes[this.val].prijs);// 1.2
     let bestlijn = new Bestellijn(broeike, this.teller);
-    console.log('aantal: ',this.teller);
-    
     this.lijst.push(bestlijn);
-    console.dir(this.lijst);
     
-//--------------------------------------------------
- //  let order = new Bestelling(bestlijn[this.val]);
- //  order.totaalprijs();
   }
   get totaal(): number {
-
-   // this.plus += this.broodjes[this.val].prijs;
-   // console.log(this.plus);
-   // this.totaal = (this.plus*this.teller);
-   return ;
+    let totaal: number = 0;
+    for (let lijn of this.lijst){
+      totaal+= lijn.broodje.prijs * lijn.aantal;
+    }
+     return totaal;
   }
 
 }
