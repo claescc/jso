5e1eff6535cd81054be4d9354ca62ae2c6b1122d
claescc <claes_cc@live.be>
2018-02-19 11:18:54 +0100

    mockheroes

--- angular/src/app/oefeningen/oefheroes/mock-heroes.ts
+++ angular/src/app/oefeningen/oefheroes/mock-heroes.ts
@@ -1,3 +1,6 @@
 import {Hero } from './hero-model/hero';
 
-export const HEROES: Hero[] = [new Hero(1, "Paulus de boskabouter"), new Hero(2, "Paula de Mier"), new Hero(3, "Meneer de Uil", false), new Hero(4, "Ben de Bever")];
+export const HEROES: Hero[] = [new Hero(1, "Paulus de boskabouter"), 
+new Hero(2, "Paula de Mier"), 
+new Hero(3, "Meneer de Uil", false), 
+new Hero(4, "Ben de Bever")];
